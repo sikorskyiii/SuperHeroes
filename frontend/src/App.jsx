987831a4001929fd74089c753614497dcd8557a5
sikorskyiii <off.sikorskyi@gmail.com>
2025-09-13 14:27:35 +0300

    feat(frontend): add React Router and basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom'
+
+function Home() {
+  return <p>Home</p>
+}
+function Create() {
+  return <p>Create</p>
+}
+
 export default function App() {
   return (
     <div className="container">
       <header className="app-header">
         <h1>🦸 SuperHeroes</h1>
+        <nav style={{display:'flex', gap:'1rem'}}>
+          <Link to="/">Список</Link>
+          <Link to="/create">Створити</Link>
+        </nav>
       </header>
       <main>
-        <p>Start Page.</p>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
       </main>
     </div>
   )
